24fd86293a16be8f55765ccf8e921f9cb411a7a8
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2018-11-19 09:47:49 -0300

    Added missing copyright

--- src/com/maxprograms/validation/Xliff11.java
+++ src/com/maxprograms/validation/Xliff11.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2003, 2018 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
+
 package com.maxprograms.validation;
 
 import java.io.File;
