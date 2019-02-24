322b3f9d2409a1dbbff4fac36325aeee44a47499
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-02-24 09:09:44 -0300

    Completed basic implemenentation

--- src/com/maxprograms/stats/SvgStats.java
+++ src/com/maxprograms/stats/SvgStats.java
@@ -12,9 +12,11 @@
 package com.maxprograms.stats;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -27,17 +29,20 @@ import com.maxprograms.converters.Utils;
 import com.maxprograms.xml.Catalog;
 import com.maxprograms.xml.Document;
 import com.maxprograms.xml.Element;
+import com.maxprograms.xml.Indenter;
 import com.maxprograms.xml.SAXBuilder;
 
 public class SvgStats {
-	
+
 	protected class SegmentStatus {
-		
+
 		private boolean approved;
-		private int match;
+		private boolean translated;
+		private float match;
 
 		public SegmentStatus() {
 			approved = false;
+			translated = false;
 			match = 0;
 		}
 
@@ -49,19 +54,27 @@ public class SvgStats {
 			this.approved = approved;
 		}
 
-		public int getMatch() {
+		public float getMatch() {
 			return match;
 		}
 
-		public void setMatch(int match) {
+		public void setMatch(float match) {
 			this.match = match;
 		}
-		
+
+		public boolean isTranslated() {
+			return translated;
+		}
+
+		public void setTranslated(boolean translated) {
+			this.translated = translated;
+		}
+
 	}
-	
+
 	private static Logger LOGGER = System.getLogger(SvgStats.class.getName());
 
-	private List<SegmentStatus>  segmentsList;
+	private List<SegmentStatus> segmentsList;
 
 	public static void main(String[] args) {
 
@@ -94,14 +107,39 @@ public class SvgStats {
 		try {
 			SvgStats instance = new SvgStats();
 			instance.analyze(file, catalog);
+
+			Element matchesSvg = instance.generateMatchesSvg();
+			Element translatedSvg = instance.generateTranslatedSvg();
+			Element approvedSvg = instance.generateApprovedSvg();
+			
+			try(FileOutputStream out = new FileOutputStream(new File(file + ".html"))) {
+				instance.writeString(out, "<!DOCTYPE html>\n");
+				instance.writeString(out, "<html>\n");
+				instance.writeString(out, "<head>\n");
+				instance.writeString(out, "</head>\n");
+				instance.writeString(out, "<body>\n");
+			
+				instance.writeString(out, "Translated Segments\n");
+				instance.writeString(out, "<br>\n");
+				instance.writeString(out, translatedSvg.toString());
+				instance.writeString(out, "<br>\n");
+				instance.writeString(out, "Approved Segments\n");
+				instance.writeString(out, "<br>\n");
+				instance.writeString(out, approvedSvg.toString());
+				instance.writeString(out, "<br>\n");
+				instance.writeString(out, "TM Matches Quality\n");
+				instance.writeString(out, "<br>\n");
+				instance.writeString(out, matchesSvg.toString());
+				
+				instance.writeString(out, "</body>\n");
+				instance.writeString(out, "</html>\n");
+			}
 		} catch (IOException | SAXException | ParserConfigurationException e) {
 			LOGGER.log(Level.ERROR, "Error analyzing file", e);
 		}
 	}
 
-	
 	public void analyze(String file, String catalog) throws SAXException, IOException, ParserConfigurationException {
-		// TODO Auto-generated method stub
 		SAXBuilder builder = new SAXBuilder();
 		builder.setEntityResolver(new Catalog(catalog));
 		Document document = builder.build(file);
@@ -111,38 +149,233 @@ public class SvgStats {
 		}
 		segmentsList = new ArrayList<>();
 		if (root.getAttributeValue("version").startsWith("1.")) {
-			parseXliff1(root);
-		} else if (root.getAttributeValue("version").startsWith("2.")) {
-			parseXliff2(root);
+			parseXliff(root);
 		} else {
 			throw new IOException("Unsupported XLIFF version");
 		}
-	}
-
-	private void parseXliff2(Element e) {
-		// TODO Auto-generated method stub
-		if ("segment".equals(e.getName())) {
-			
+		if (segmentsList.isEmpty()) {
+			throw new IOException("Empty XLIFF");
 		}
-		List<Element> children = e.getChildren();
-		Iterator<Element> it = children.iterator();
-		while (it.hasNext()) {
-			parseXliff1(it.next());
-		}		
+		
 	}
 
-	private void parseXliff1(Element e) {
+	private void writeString(FileOutputStream out, String string) throws IOException {
+		out.write(string.getBytes(StandardCharsets.UTF_8));
+	}
+
+	private Element basicSvg() {
+		Element svg = new Element("svg");
+		svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+		svg.setAttribute("width", "640px");
+		svg.setAttribute("height", "130px");
+
+		Element rect = new Element("rect");
+		rect.setAttribute("width", "640px");
+		rect.setAttribute("height", "130px");
+		rect.setAttribute("style", "fill:white;");
+		svg.addContent(rect);
+
+		Element xAxe = new Element("line");
+		xAxe.setAttribute("x1", "5");
+		xAxe.setAttribute("y1", "110");
+		xAxe.setAttribute("x2", "615");
+		xAxe.setAttribute("y2", "110");
+		xAxe.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:2;");
+		svg.addContent(xAxe);
+
+		for (int x = 0; x < 20; x++) {
+			Element pipe = new Element("line");
+			pipe.setAttribute("x1", "" + (40 + x * 30));
+			pipe.setAttribute("x2", "" + (40 + x * 30));
+			if (x % 2 == 0) {
+				pipe.setAttribute("y1", "108");
+				pipe.setAttribute("y2", "112");
+			} else {
+				pipe.setAttribute("y1", "106");
+				pipe.setAttribute("y2", "114");
+			}
+			pipe.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:1;");
+			svg.addContent(pipe);
+		}
+		int size = segmentsList.size() * 10 / 1000;
+		for (int x = 10; x <= 100; x += 10) {
+			Element text = new Element("text");
+			text.setText(x * size + "");
+			text.setAttribute("style", "font-family:sans-serif;font-size:10px;");
+			text.setAttribute("x", "" + +x * 6);
+			text.setAttribute("y", "122");
+			svg.addContent(text);
+		}
+
+		Element yAxe = new Element("line");
+		yAxe.setAttribute("x1", "10");
+		yAxe.setAttribute("y1", "5");
+		yAxe.setAttribute("x2", "10");
+		yAxe.setAttribute("y2", "115");
+		yAxe.setAttribute("style", "stroke:rgb(0,0,0);stroke-width:2;");
+		svg.addContent(yAxe);
+
+		Element yTop = new Element("line");
+		yTop.setAttribute("x1", "10");
+		yTop.setAttribute("y1", "10");
+		yTop.setAttribute("x2", "610");
+		yTop.setAttribute("y2", "10");
+		yTop.setAttribute("style", "stroke:grey;stroke-width:1;stroke-dasharray:5,5;");
+		svg.addContent(yTop);
+
+		return svg;
+	}
+
+	private Element generateMatchesSvg() {
+		Element svg = basicSvg();
+
+		Element text100 = new Element("text");
+		text100.setText("100%");
+		text100.setAttribute("style", "font-family:sans-serif;font-size:10px;");
+		text100.setAttribute("x", "612");
+		text100.setAttribute("y", "15");
+		svg.addContent(text100);
+
+		Element yCenter = new Element("line");
+		yCenter.setAttribute("x1", "10");
+		yCenter.setAttribute("y1", "60");
+		yCenter.setAttribute("x2", "610");
+		yCenter.setAttribute("y2", "60");
+		yCenter.setAttribute("style", "stroke:grey;stroke-width:1;stroke-dasharray:5,5;");
+		svg.addContent(yCenter);
+
+		Element text50 = new Element("text");
+		text50.setText("50%");
+		text50.setAttribute("style", "font-family:sans-serif;font-size:10px;");
+		text50.setAttribute("x", "612");
+		text50.setAttribute("y", "65");
+		svg.addContent(text50);
+
+		int size = segmentsList.size() * 10 / 2000;
+		for (int x = 0; x < 200; x++) {
+			float y = matchAverage(x, size);
+			if (y < 1) {
+				continue;
+			}
+			Element line = new Element("line");
+			line.setAttribute("x1", "" + (12 + 3 * x));
+			line.setAttribute("y1", "" + (110 - y));
+			line.setAttribute("x2", "" + (12 + 3 * x));
+			line.setAttribute("y2", "110");
+			line.setAttribute("style", "stroke:blue;stroke-width:1;");
+			svg.addContent(line);
+		}
+		Indenter.indent(svg, 2);
+		return svg;
+	}
+
+	private Element generateTranslatedSvg() {
+		Element svg = basicSvg();
+
+		int size = segmentsList.size() * 10 / 2000;
+		for (int x = 0; x < 200; x++) {
+			float y = translatedAverage(x, size);
+			if (y < 1) {
+				continue;
+			}
+
+			Element line = new Element("line");
+			line.setAttribute("x1", "" + (12 + 3 * x));
+			line.setAttribute("y1", "" + (110 - y));
+			line.setAttribute("x2", "" + (12 + 3 * x));
+			line.setAttribute("y2", "110");
+			line.setAttribute("style", "stroke:green;stroke-width:2;");
+			svg.addContent(line);
+		}
+		Indenter.indent(svg, 2);
+		return svg;
+	}
+
+	private Element generateApprovedSvg() {
+		Element svg = basicSvg();
+
+		int size = segmentsList.size() * 10 / 2000;
+		for (int x = 0; x < 200; x++) {
+			float y = approvedAverage(x, size);
+			if (y < 1) {
+				continue;
+			}
+
+			Element line = new Element("line");
+			line.setAttribute("x1", "" + (12 + 3 * x));
+			line.setAttribute("y1", "" + (110 - y));
+			line.setAttribute("x2", "" + (12 + 3 * x));
+			line.setAttribute("y2", "110");
+			line.setAttribute("style", "stroke:red;stroke-width:2;");
+			svg.addContent(line);
+		}
+		Indenter.indent(svg, 2);
+		return svg;
+	}
+
+	private float translatedAverage(int x, int size) {
+		float total = 0;
+		int counted = 0;
+		for (int count = 0; count < size; count++) {
+			if (x * size + count < segmentsList.size()) {
+				total += segmentsList.get(x * size + count).isTranslated() ? 1 : 0;
+				counted++;
+			}
+		}
+		if (counted == 0) {
+			return 0;
+		}
+		return total > counted/2 ? 100 : 0;
+	}
+
+	private float approvedAverage(int x, int size) {
+		float total = 0;
+		int counted = 0;
+		for (int count = 0; count < size; count++) {
+			if (x * size + count < segmentsList.size()) {
+				total += segmentsList.get(x * size + count).isApproved() ? 1 : 0;
+				counted++;
+			}
+		}
+		if (counted == 0) {
+			return 0;
+		}
+		return total > counted/2 ? 100 : 0;
+	}
+
+	private float matchAverage(int x, int size) {
+		float total = 0;
+		int counted = 0;
+		for (int count = 0; count < size; count++) {
+			if (x * size + count < segmentsList.size()) {
+				total += segmentsList.get(x * size + count).getMatch();
+				counted++;
+			}
+		}
+		if (counted == 0) {
+			return 0;
+		}
+		return total / counted;
+	}
+
+	private void parseXliff(Element e) {
 		if ("trans-unit".equals(e.getName())) {
 			SegmentStatus status = new SegmentStatus();
 			status.setApproved(e.getAttributeValue("approved", "no").equalsIgnoreCase("yes"));
+			Element target = e.getChild("target");
+			if (target == null) {
+				status.setTranslated(false);
+			} else {
+				status.setTranslated(!target.getContent().isEmpty());
+			}
 			List<Element> matches = e.getChildren("alt-trans");
 			if (!matches.isEmpty()) {
-				int max = 0;
+				float max = 0;
 				Iterator<Element> it = matches.iterator();
 				while (it.hasNext()) {
 					Element match = it.next();
 					try {
-						int quality = Integer.parseInt(match.getAttributeValue("match-quality", "0"));
+						float quality = Float.parseFloat(match.getAttributeValue("match-quality", "0"));
 						if (quality > max) {
 							max = quality;
 						}
@@ -160,8 +393,8 @@ public class SvgStats {
 		List<Element> children = e.getChildren();
 		Iterator<Element> it = children.iterator();
 		while (it.hasNext()) {
-			parseXliff1(it.next());
+			parseXliff(it.next());
 		}
 	}
-	
+
 }
