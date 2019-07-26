bddd767b0251646e6bf1f8c8db6e6636854d59b6
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-07-26 20:24:06 -0300

    Added option to check server url

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
 	public static final String VERSION = "1.4.1";
-	public static final String BUILD = "20190725_1345";
+	public static final String BUILD = "20190726_2015";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
