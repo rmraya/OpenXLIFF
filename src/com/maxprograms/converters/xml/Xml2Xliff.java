a9aa3acbacb209c84805732dba881a9519910b54
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-07-30 12:42:37 -0300

    Removed unused import

--- src/com/maxprograms/converters/xml/Xml2Xliff.java
+++ src/com/maxprograms/converters/xml/Xml2Xliff.java
@@ -20,7 +20,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Enumeration;
