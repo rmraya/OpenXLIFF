f6969b5a88e96f6f299dd388eb3e7213e1952471
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2019-12-18 08:56:34 -0300

    Aupdated list of language tags

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -19,8 +19,8 @@ public class Constants {
 
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
-	public static final String VERSION = "1.6.0";
-	public static final String BUILD = "20191201_1444";
+	public static final String VERSION = "1.6.1";
+	public static final String BUILD = "20191218_0854";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
