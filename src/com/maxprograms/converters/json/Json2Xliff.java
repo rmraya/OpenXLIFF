e836fb1fd47321536a162699658f2ab0a33a526d
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2020-10-07 11:28:29 -0300

    Added missing copyright

--- src/com/maxprograms/converters/json/Json2Xliff.java
+++ src/com/maxprograms/converters/json/Json2Xliff.java
@@ -1,4 +1,16 @@
-package com.maxprograms.converters.json;
+/*******************************************************************************
+ * Copyright (c) 2003-2020 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
+
+ package com.maxprograms.converters.json;
 
 import java.io.BufferedReader;
 import java.io.File;
