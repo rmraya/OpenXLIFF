e293ce3fda6ba352e2c1940c74e647eb1ff109b5
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2021-03-25 07:26:26 -0300

    Added new Languages to DeeL

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -19,8 +19,8 @@ public class Constants {
 
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
-	public static final String VERSION = "1.12.5";
-	public static final String BUILD = "202140203_0840";
+	public static final String VERSION = "1.12.6";
+	public static final String BUILD = "202140325_0724";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
