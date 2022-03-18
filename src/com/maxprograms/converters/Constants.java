1d17072071ca3db966d0d5af5bf6cb1a00efb6d4
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2022-03-18 11:36:31 -0300

    Updated language list

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
 	public static final String VERSION = "1.17.5";
-	public static final String BUILD = "20220317_1438";
+	public static final String BUILD = "20220318_1135";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
