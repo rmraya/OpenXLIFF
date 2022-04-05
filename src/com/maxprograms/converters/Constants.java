dee60c0a08629918befaa52bdeaa3b4a673b3a81
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2022-04-05 08:39:17 -0300

    Expanded Serbian language codes

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
 	public static final String VERSION = "2.0.0";
-	public static final String BUILD = "20220329_1318";
+	public static final String BUILD = "20220405_0837";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
