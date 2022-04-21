c570d57292556cb2acdef6870f1c114728f5c164
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2022-04-21 09:28:28 -0300

    Updated version number

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -19,8 +19,8 @@ public class Constants {
 
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
-	public static final String VERSION = "2.0.0";
-	public static final String BUILD = "20220416_0848";
+	public static final String VERSION = "2.1.0";
+	public static final String BUILD = "20220421_0927";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
