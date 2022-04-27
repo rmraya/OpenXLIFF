e6e2c707650cef7c5207409283abb6868eab5871
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2022-04-27 13:55:53 -0300

    Added copyright notice

--- src/com/maxprograms/converters/PseudoTranslation.java
+++ src/com/maxprograms/converters/PseudoTranslation.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Maxprograms.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
+ *
+ * Contributors:
+ *     Maxprograms - initial API and implementation
+ *******************************************************************************/
 package com.maxprograms.converters;
 
 import java.io.File;
