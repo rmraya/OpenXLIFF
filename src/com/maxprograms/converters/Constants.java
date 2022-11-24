af0fef4e6f0ff5a8a1536d46f4aecbab082c69de
Rodolfo M. Raya <rmraya@maxprograms.com>
2022-11-24 09:44:22 -0300

    Exported additional packages

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
 	public static final String VERSION = "2.10.0";
-	public static final String BUILD = "20221122_1135";
+	public static final String BUILD = "20221123_0856";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
