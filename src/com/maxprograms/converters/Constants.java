0732d59f4456dafddf9d24c35db7b3706840b55e
Rodolfo M. Raya <rmraya@maxprograms.com>
2023-06-06 10:05:43 -0300

    Updated XMLJava

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
 	public static final String VERSION = "3.8.0";
-	public static final String BUILD = "20230602_0749";
+	public static final String BUILD = "20230606_1005";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
