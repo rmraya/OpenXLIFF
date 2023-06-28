d7d9e95b45367966f41c089573f1c1d633583554
Rodolfo M. Raya <rmraya@maxprograms.com>
2023-06-28 12:38:33 -0300

    Trimmed quotes from ChatGPT translations

--- src/com/maxprograms/mt/ChatGptTranslator.java
+++ src/com/maxprograms/mt/ChatGptTranslator.java
@@ -137,6 +137,9 @@ public class ChatGptTranslator implements MTEngine {
             if (translation.startsWith("\n\n")) {
                 translation = translation.substring(2);
             }
+            if (translation.startsWith("\"") && translation.endsWith("\"")) {
+                translation = translation.substring(1, translation.length() - 1);
+            }
             return translation;
         }
         con.disconnect();
