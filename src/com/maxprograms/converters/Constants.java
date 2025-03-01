7816eb893eb5ecff7d931df42614c69fc9490e99
Rodolfo M. Raya <rmraya@maxprograms.com>
2025-03-01 08:32:13 -0300

    Adjusted QTI configuration

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -21,8 +21,8 @@ public class Constants {
 
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
-	public static final String VERSION = "4.3.0";
-	public static final String BUILD = "20250227_1031";
+	public static final String VERSION = "4.3.1";
+	public static final String BUILD = "20250301_0831";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
