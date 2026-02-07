d6702a34d83a8a446c5ffaf97132aadb8f4a523f
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-02-06 21:16:17 -0300

    Updated copyright year

--- src/com/maxprograms/converters/Messages.java
+++ src/com/maxprograms/converters/Messages.java
@@ -1,11 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2018 - 2025 Maxprograms.
+ * Copyright (c) 2018 - 2026 Maxprograms.
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 1.0 which accompanies this distribution,
- * and is available at https://www.eclipse.org/org/documents/epl-v10.html
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/org/documents/epl-v10.html
  *
- * Contributors: Maxprograms - initial API and implementation
+ * Contributors:
+ *     Maxprograms - initial API and implementation
  *******************************************************************************/
 package com.maxprograms.converters;
 
