5c19f98299c9c9ec6899c87c161ad0d690095717
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-03-17 14:57:59 -0300

    Fixed detection of QTI files

--- src/com/maxprograms/converters/Constants.java
+++ src/com/maxprograms/converters/Constants.java
@@ -21,8 +21,8 @@ public class Constants {
 
 	public static final String TOOLID = "OpenXLIFF";
 	public static final String TOOLNAME = "OpenXLIFF Filters";
-	public static final String VERSION = "5.0.1";
-	public static final String BUILD = "20260310_1837";
+	public static final String VERSION = "5.0.2";
+	public static final String BUILD = "20260317_1453";
 
 	public static final String SUCCESS = "0";
 	public static final String ERROR = "1";
